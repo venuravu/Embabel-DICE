2de15b5ff08da5a9bdf2a64284374e3017a30887
Venugopal Ravu <venugopalarao.r@gmail.com>
2026-03-04 16:21:32 +0530

    Initial commit

---
+++ src/main/java/com/embabel/dice/app/DiceApplication.java
@@ -0,0 +1,11 @@
+package com.embabel.dice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiceApplication.class, args);
+    }
+}
